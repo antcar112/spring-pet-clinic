b5b7ab6f0baca9be0929c646f452fff476b23dae
Anthony Caron <antcar1992@gmail.com>
2020-11-28 11:09:56 -0800

    Added POJOs. Closes #2

---
+++ src/main/java/antcar/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package antcar.springframework.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/antcar/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package antcar.springframework.springpetclinic.model;
+
+public class Vet extends Person {
+}
