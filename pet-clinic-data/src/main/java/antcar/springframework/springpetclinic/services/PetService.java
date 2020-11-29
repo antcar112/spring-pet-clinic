d6ebe3ae13025c51102c7806be5a4c2428e90699
Anthony Caron <antcar1992@gmail.com>
2020-11-29 11:26:13 -0800

    Added service interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/antcar/springframework/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package antcar.springframework.springpetclinic.services;
+
+import antcar.springframework.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/antcar/springframework/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package antcar.springframework.springpetclinic.services;
+
+import antcar.springframework.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
