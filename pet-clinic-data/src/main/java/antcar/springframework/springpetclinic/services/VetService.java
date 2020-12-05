a4588006cfae9e36f4c99727a7c64316b42f47d9
Anthony Caron <antcar1992@gmail.com>
2020-12-04 20:00:21 -0800

    Refactored to common crud interface. closes #11

--- pet-clinic-data/src/main/java/antcar/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/antcar/springframework/springpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package antcar.springframework.springpetclinic.services;
 
 import antcar.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>  {
 }
