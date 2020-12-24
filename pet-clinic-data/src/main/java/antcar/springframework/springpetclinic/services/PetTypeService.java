40c76a48f4504ae7de550b53d4b0818c7ee3d57a
Anthony Caron <antcar1992@gmail.com>
2020-12-24 11:14:42 -0800

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/antcar/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package antcar.springframework.springpetclinic.services;
+
+import antcar.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
